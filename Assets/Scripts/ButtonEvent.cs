ec3a8a8f2d5e81a926f001323cb459e04234784f
werqueski <65481590+werqueski@users.noreply.github.com>
2021-12-17 10:28:59 +0800

    Add files via upload

---
+++ Assets/Scripts/ButtonEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonEvent : MonoBehaviour
+{
+    public Roll roll;
+
+    public void ButtonClick(){
+        roll.ButtonClick();
+    }
+}
